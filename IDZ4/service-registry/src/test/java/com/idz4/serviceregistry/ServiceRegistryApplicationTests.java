75d4dbb4c10eb428754bf9dbf3dbeb1704a3cb3c
Raaazzy <111382627+Raaazzy@users.noreply.github.com>
2023-06-18 22:31:23 +0300

    Add files via upload

---
+++ IDZ4/service-registry/src/test/java/com/idz4/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idz4.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
